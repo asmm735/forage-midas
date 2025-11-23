8e0193273022799a5e4ec90e9d6a7e6523f5dc6e
Asmita Pal <asmita735pal@gmail.com>
2025-11-23 12:11:04 +0530

    Fix circular reference in TransactionListener - use direct property injection

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,17 +1,13 @@
 package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.foundation.Transaction;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionListener {
 
-    @Value("${general.kafka-topic}")
-    private String kafkaTopic;
-
-    @KafkaListener(topics = "#{@transactionListener.kafkaTopic}", groupId = "midas-core")
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
     public void listen(Transaction transaction) {
         // TODO: Process transaction in later tasks
         System.out.println("Received transaction: " + transaction);
