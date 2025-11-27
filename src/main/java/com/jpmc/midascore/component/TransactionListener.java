8762ef38a03e67fbb9b8f1d7f93db832f176492f
Asmita Pal <asmita735pal@gmail.com>
2025-11-27 21:29:17 +0530

    Integrate TransactionService in TransactionListener
    Added TransactionService to process transactions in the listener.

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -3,13 +3,16 @@ package com.jpmc.midascore.component;
 import com.jpmc.midascore.foundation.Transaction;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import com.jpmc.midascore.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 public class TransactionListener {
 
+        @Autowired
+    private TransactionService transactionService;
+
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
     public void listen(Transaction transaction) {
-        // TODO: Process transaction in later tasks
-        System.out.println("Received transaction: " + transaction);
-    }
+        transactionService.processTransaction(transaction);    }
 }
